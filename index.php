6a63d89eb9bccb1e79ceefebe735e6a92c544576
Dadiku <69049111+Dadiku@users.noreply.github.com>
2021-06-28 09:05:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,26 +3,3 @@
 echo "test by echo";
 
 ?>
-
-<html>
-
-<head>
-<style>
-body
-{
-	text-align: center;
-	font-size: 150px;
-	font-family: verdana;
-	color: white;
-	background-color: black;
-}
-</style>
-</head>
-
-<body>
-	
-	
-	
-</body>
-
-</html>
